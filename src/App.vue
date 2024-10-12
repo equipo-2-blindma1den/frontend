71a863f7521b0904edab3ce5449268ec475b0522
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-10-12 17:22:09 -0400

    Add main layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { Button } from "@/components/ui/button"
+import Layout from "./components/Layout.vue"
 </script>
 
 <template>
-  <div>
-    <Button>Click me</Button>
+  <div class="min-h-screen w-full max-w-7xl">
+    <Layout>
+      <Button>Click me</Button>
+    </Layout>
   </div>
 </template>
---
+++ src/components/Layout.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="lg:px-20 md:px-10 px-5">
+    <slot />
+  </div>
+</template>
