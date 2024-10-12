24036185a5da25abdc605fed097c578a7be4666e
Freida Portillo <alejandrabypoca@gmail.com>
2024-10-12 15:33:53 -0600

    feat: add ionic

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
+import { IonicVue } from '@ionic/vue';
 import './style.css'
 import App from './App.vue'
 import router from './router';
 
+import '@ionic/vue/css/typography.css';
+
 createApp(App)
+    .use(IonicVue)
     .use(router)
     .mount('#app')
