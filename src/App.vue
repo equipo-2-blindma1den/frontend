8e19071c1b084a42721389fe065233c7b6ebf908
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-10-12 19:49:04 -0400

    Create card

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
 import Header from "./components/Header.vue"
 import Layout from "./components/Layout.vue"
+import Card from "./components/Card.vue"
 </script>
 
 <template>
   <div class="min-h-screen w-full">
     <Header />
     <Layout>
+      <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 py-5">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </Layout>
   </div>
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+
+const contentText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio sunt exercitationem omnis deleniti dignissimos cum mollitia nemo quas. Error sint magni dicta repudiandae blanditiis culpa voluptate consectetur veniam nihil ipsa."
+</script>
+
+<template>
+  <Card>
+    <CardHeader>
+      <img class="max-w-full h-auto object-cover aspect-video" src="https://github.com/shadcn.png" alt="avatar" />
+      <CardTitle>Donaciones Huracán Milton</CardTitle>
+      <CardDescription>Aporta tu grano de arena a la donación por las víctimas</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <p>
+        {{ contentText.substring(0, 100) + "..." }}
+      </p>
+    </CardContent>
+    <CardFooter>
+      <Button>Más información</Button>
+    </CardFooter>
+  </Card>
+</template>
