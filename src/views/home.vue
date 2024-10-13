d5e351a0d54cb9b9182d53deda923a178131cc5e
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-10-13 12:06:36 -0400

    Create buttons for filtering

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,9 +3,15 @@ import { onMounted, ref } from "vue"
 import Header from "@/components/shared/Header.vue"
 import Layout from "@/components/shared/Layout.vue"
 import Card from "@/components/shared/Card.vue"
+import { Button } from '@/components/ui/button';
 import { EventsService } from "../services/events.js"
 
 const events = ref([])
+const activeButton = ref({
+  'Todos': true,
+  'Donaciones': false,
+  'Voluntariados': false
+})
 
 onMounted(async () => {
   await EventsService.getAllEvents().then(data => {
@@ -18,6 +24,16 @@ onMounted(async () => {
 <template>
   <Header />
   <Layout>
+    <div class="flex gap-2 pt-5">
+      <Button
+        v-for="(button, index) in activeButton"
+        :key="index"
+        :variant="button ? '' : 'secondary'"
+        @click="activeButton[index] = !activeButton[index]"
+      >
+        {{ index }}
+      </Button>
+    </div>
     <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 py-5">
       <Card
         v-for="event in events.data"
