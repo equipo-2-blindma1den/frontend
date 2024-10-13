ba5da5b38b39897c0d86d709b73b285cf7af73c1
Freida Portillo <alejandrabypoca@gmail.com>
2024-10-13 06:37:40 -0600

    fix merge conflicts

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import login from '../views/login.vue';
-// import Login from '../views/Login.vue';
-import app from '../views/app.vue';
 import home from '../views/home.vue';
 import register from '../views/complete-register.vue';
 import profile from '../views/profile.vue';
@@ -11,7 +9,6 @@ import { SessionService } from '@/services/session';
 const routes = [
   { path: '/',  redirect: '/login'},
   { path: '/login', component: login },
-  { path: '/app', component: app },
   { 
     path: '/app',
     redirect: '/app/home',
