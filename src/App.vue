b1363d44d86e0a84905c7851a2d2c55a42d03fd1
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-10-13 06:34:03 -0400

    Adapt home into a view

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,5 @@
-<script setup>
-import { onMounted, ref } from "vue"
-import Header from "./components/Header.vue"
-import Layout from "./components/Layout.vue"
-import Card from "./components/Card.vue"
-import { EventsService } from "./services/events.js"
-
-const events = ref([])
-
-onMounted(async () => {
-  await EventsService.getAllEvents().then(data => {
-    events.value = data
-  })
-})
-</script>
-
 <template>
   <div class="min-h-screen w-full">
-    <Header />
-    <Layout>
-      <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 py-5">
-        <Card
-          v-for="event in events.data"
-          :key="event.id"
-          :event="event"
-        />
-      </div>
-    </Layout>
     <router-view></router-view>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import login from '../views/login.vue';
 // import Login from '../views/Login.vue';
+import app from '../views/app.vue';
 
 const routes = [
   { path: '/', component: login },
   { path: '/login', component: login },
-  { path: '/app', component: login },
+  { path: '/app', component: app },
 ];
 
 const router = createRouter({
