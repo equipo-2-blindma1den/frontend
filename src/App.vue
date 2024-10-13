0a02f4d565de91808dd52efbaa5be672849ab614
Freida Portillo <alejandrabypoca@gmail.com>
2024-10-13 03:16:13 -0600

    feat: add api service and session storage

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <script setup>
-import Header from "./components/Header.vue"
-import Layout from "./components/Layout.vue"
-import Card from "./components/Card.vue"
+
 </script>
 
 <template>
-  <div class="min-h-screen w-full">
-    <Header />
-    <Layout>
-      <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 py-5">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    </Layout>
-    <router-view></router-view>
-  </div>
+  <router-view></router-view>
+</template>
--- src/services/session.js
+++ src/services/session.js
@@ -1,7 +1,9 @@
 import { useAlertStore } from '@/store/alert.js';
+import { useSessionStore } from '@/store/session.js';
 import { GlobalVars } from '@/globalVars.js';
 
 const alertStore = useAlertStore();
+const sessionStore = useSessionStore();
 
 const login = async ({username, password}) => {
     try {
@@ -12,16 +14,17 @@ const login = async ({username, password}) => {
             },
         });
         
-        if (!response.ok) {
-            const message = response?.message || 'Hubo un error con el servidor, intenta más tarde.'
+        const data = await response?.json(); 
+        if (!response.ok  || !data.success) {
+            const message = data?.message || 'Hubo un error con el servidor, intenta más tarde.'
             alertStore.showAlert(message, 'Error', 'destructive');
             throw new Error('Error al enviar los datos');
         }
-
-        // emit('loginSuccess', data);
-        const data = await response.json(); // Parsear la respuesta JSON
+        
         console.log('Respuesta del servidor:', data);
+        if(data.data) sessionStore.save(data.data);
     } catch (error) {
+        console.log(error);
         const message = error?.message || 'Hubo un error con el servidor, intenta más tarde.'
         alertStore.showAlert(message, 'Error', 'destructive');
         throw new Error('Error al enviar los datos');
@@ -29,26 +32,62 @@ const login = async ({username, password}) => {
 }
 
 const register = async ({username, password, city}) => {
-    username =  'jc_perez';
-    password = 'password123';
+    city = 'madrid'
     try {
-        const response = await fetch(`${GlobalVars.API_URL}/signup`, {
+        const response = await fetch(`${GlobalVars.API_URL}signup`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ usuario: username, password})
+            body: JSON.stringify({ usuario: username, password, ciudad: city})
         });
 
-        if (!response.ok) {
-            const message = response?.message || 'Hubo un error con el servidor, intenta más tarde.'
+        const data = await response?.json(); 
+        if (!response.ok  || !data.success) {
+            const message = data?.message || 'Hubo un error con el servidor, intenta más tarde.'
             alertStore.showAlert(message, 'Error', 'destructive');
             throw new Error('Error al enviar los datos');
         }
 
-        // emit('loginSuccess', data);
-        const data = await response.json(); // Parsear la respuesta JSON
         console.log('Respuesta del servidor:', data);
+        sessionStore.save({ ...data.data, usuario: username, password, ciudad: city});
+    } catch (error) {
+        const message = error?.message || 'Hubo un error con el servidor, intenta más tarde.'
+        alertStore.showAlert(message, 'Error', 'destructive');
+        throw new Error('Error al enviar los datos');
+    }
+}
+
+const update = async ({name, name2, lastname, lastname2, sex, username, city}) => {
+    try {
+        const response = await fetch(`${GlobalVars.API_URL}updateUsuario`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "id_usuario": sessionStore.state.idUser,
+                "nombre": name,
+                "segundo_nombre": name2,
+                "ciudad": city,
+                "apellido_p":lastname,
+                "apellido_m":lastname2,
+                "usuario":username,
+                "password":sessionStore.state.password,
+                "sexo_genero":sex
+            })
+        });
+
+        const data = await response?.json(); 
+        if (!response.ok  || !data.success) {
+            const message = data?.message || 'Hubo un error con el servidor, intenta más tarde.'
+            alertStore.showAlert(message, 'Error', 'destructive');
+            throw new Error('Error al enviar los datos');
+        }
+        
+        console.log('Respuesta del servidor:', data);
+        sessionStore.save(data.data);
+        alertStore.showAlert('Tu información se actualizó correctamente.', '¡Genial!', 'default');
     } catch (error) {
         const message = error?.message || 'Hubo un error con el servidor, intenta más tarde.'
         alertStore.showAlert(message, 'Error', 'destructive');
@@ -58,5 +97,6 @@ const register = async ({username, password, city}) => {
 
 export const SessionService = {
     login,
-    register
+    register,
+    update
 }
